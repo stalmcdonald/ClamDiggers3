862cbd507ad08d0e81de13fd1ee185f3a445a1d4
Crystal McDonald <stalmcdonald@fullsail.edu>
2013-09-15 13:08:58 -0700

    Implicit Intent added
    web intent provides info on how to dig for razor clams

---
+++ src/com/cm/clamdiggers3/SecondActivity.java
@@ -0,0 +1,66 @@
+package com.cm.clamdiggers3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.cm.clamdiggers3.R;
+
+public class SecondActivity extends Activity implements OnClickListener {
+	Button goTide;
+	Button browse;
+	EditText cityEntered;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tide);
+		cityEntered = (EditText)findViewById(R.id.cityText);
+		goTide = (Button)findViewById(R.id.tideButton);
+		
+		//set listener for browser button
+		browse = (Button)findViewById(R.id.webButton);
+		
+		goTide.setOnClickListener(this);
+		
+		//Implicit Intent
+		browse.setOnClickListener(new OnClickListener() {
+		@Override
+		public void onClick(View interwebs) {
+			// sends user to site that shows more information on forecast
+			Uri uriUrl = Uri.parse("http://wdfw.wa.gov/fishing/shellfish/razorclams/howto_dig.html");
+			
+			Intent browse_intent = new Intent(Intent.ACTION_VIEW, uriUrl);
+			
+			startActivity(browse_intent);
+			
+		}
+	});
+
+	
+
+}
+
+	//Activity 2 takes in a city and displays it on activity 1
+	@Override
+	public void onClick(View v) {
+		// get info from edit text and send back
+		String cityInfo = cityEntered.getText().toString();
+		//get info from intent that opens the class
+		Intent i = getIntent ();
+		//looking for i.putExtras
+		String msg = i.getStringExtra("cityEnter");
+		//Takes the result and closes the page and appends to MainActivity in its' button
+		if(msg.contentEquals("city")){
+			i.putExtra("enteredInfo", cityInfo);
+			setResult(RESULT_OK, i);
+			finish();
+		}
+		
+	}
+}//end
--- src/com/cm/clamdiggers3/TideActivity.java
+++ src/com/cm/clamdiggers3/TideActivity.java
@@ -7,16 +7,32 @@
  */
 package com.cm.clamdiggers3;
 
+import com.cm.clamdiggers3.R;
+import com.cm.clamdiggers3.SecondActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class TideActivity extends Activity {
-
+public class TideActivity extends Activity implements OnClickListener{
+	Button city, predict;
+	TextView tidalPrediction;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tide);
+		city = (Button) findViewById(R.id.cButton);
+		predict = (Button) findViewById(R.id.pButton);
+		tidalPrediction = (TextView) findViewById(R.id.textPrediction);
+		
+		//setup onclick Listener
+		city.setOnClickListener(this);
+		predict.setOnClickListener(this);
 	}
 
 	@Override
@@ -26,4 +42,47 @@ public class TideActivity extends Activity {
 		return true;
 	}
 
-}
+	//Explicit Intent
+	@Override
+	public void onClick(View v) {
+		// if city button pressed go to new activity
+		/*Intent i = new Intent(this, SecondActivity.class);
+		i.putExtra("cityEnter", "city");
+		startActivity(i);*/
+		
+		//Log.i("Entered:", city);
+		//Add City to url
+		//String baseURL =  "http://api.wunderground.com/api/3e64fa36c4f09bdd/tide/q/FL/"+cities+".json" 
+		//string myCity = 
+		
+		Intent i = new Intent(this, SecondActivity.class);
+		switch (v.getId()){
+		
+		//buttons
+		case R.id.cButton:
+			
+			//city button
+			i.putExtra("cityEnter", "city");
+			startActivityForResult(i,1);
+			break;
+		case R.id.pButton:
+		
+		//predict button
+			final String baseURL = "http://api.wunderground.com/api/3e64fa36c4f09bdd/tide/q/";
+			String c = city.getText().toString();
+			String p = predict.getText().toString();
+			String t = tidalPrediction.getText().toString();
+		}
+		
+	}
+	
+	//Retrieves state from second activity to be added to url
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		super.onActivityResult(requestCode, resultCode, data);
+		if(data.getExtras().containsKey("enteredInfo")){
+			city.setText(data.getStringExtra("enteredInfo"));
+		}
+	}
+
+}//end
